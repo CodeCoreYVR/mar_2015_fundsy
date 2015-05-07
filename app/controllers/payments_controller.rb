5b45d4494f61921365d365e60c962ebf85ccee8f
Tammam Kbeili <tammam.kbeili@gmail.com>
2015-05-07 13:31:17 -0700

    refactor payments create to a service

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,39 +1,30 @@
 class PaymentsController < ApplicationController
   before_action :authenticate_user!
   before_action :find_pledge
+  before_action :authorize_pledge
 
   def new
   end
 
   def create
-    customer = Stripe::Customer.create(
-                :description => "Customer for #{current_user.email}",
-                :source => params[:stripe_token]
-              )
-    current_user.stripe_customer_token = customer.id
-    current_user.stripe_last4 = customer.cards.data[0].last4
-    current_user.stripe_card_type = customer.cards.data[0].brand
-
-    current_user.save
-
-    charge = Stripe::Charge.create(
-      :amount => @pledge.amount * 100,
-      :currency => "cad",
-      :customer => current_user.stripe_customer_token, 
-      :description => "Pledge for #{@pledge.campaign.title}"
-    )
-
-    @pledge.stripe_txn_id = charge.id
-    @pledge.pay
-    @pledge.save
-
-    redirect_to @pledge.campaign, notice: "Thank you for Pledging"
+    service = Payments::CreatePayment.new(user: current_user,
+                                          stripe_token: params[:stripe_token],
+                                          pledge: @pledge)
+    if service.call
+      redirect_to @pledge.campaign, notice: "Thank you for Pledging"
+    else
+      render :new
+    end
   end
 
   private
 
   def find_pledge
     @pledge = Pledge.find params[:pledge_id]
+  end
+
+  def authorize_pledge
+    redirect_to @pledge.campaign, notice: "Already paid" if @pledge.paid?
   end
 
 end
---
+++ app/services/payments/create_payment.rb
@@ -0,0 +1,52 @@
+class Payments::CreatePayment
+
+  include Virtus.model
+
+  attribute :stripe_token, String
+  attribute :user, User
+  attribute :pledge, Pledge
+
+  def call
+    ActiveRecord::Base.transaction do
+      begin
+        update_user_info && charge && update_pledge
+      rescue => e
+        Rails.logger.info "Exception happened #{e.message}"
+        raise ActiveRecord::Rollback.new
+      end
+    end
+  end
+
+  private
+
+  def customer
+    @customer ||= Stripe::Customer.create(
+                      description: "Customer for #{user.email}",
+                      source: stripe_token
+                    )
+  end
+
+  def update_user_info
+    user.stripe_customer_token = customer.id
+    user.stripe_last4         = customer.sources.data[0].last4
+    user.stripe_card_type     = customer.sources.data[0].brand
+    user.save
+  end
+
+  def charge
+    charge = Stripe::Charge.create(
+      amount: pledge.amount * 100,
+      currency: "cad",
+      customer: user.stripe_customer_token, 
+      description: "Pledge for #{@pledge.campaign.title}"
+    )
+  end
+
+  def update_pledge
+    pledge.stripe_txn_id = charge.id
+    pledge.pay
+    pledge.save
+  end
+
+
+end
